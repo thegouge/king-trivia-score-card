cfb06336a95e7253bea75f57cd5a37a5c38248a8
thegouge <thegouge42@gmail.com>
2020-09-03 15:05:52 -0700

    fixing some missing timer things

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -4,17 +4,28 @@
       <h2>Set your timer!</h2>
 
       <label for="minutes">Minutes</label>
-      <input type="number" id="minutes" v-model="this.minutesLeft">
+      <input
+        type="number"
+        id="minutes"
+        v-model="minutesLeft"
+        @focus="($event) => $event.target.select()"
+      />
 
       <label for="seconds">Seconds</label>
-      <input type="number" id="seconds" v-model="this.secondsLeft">
+      <input
+        type="number"
+        id="seconds"
+        v-model="secondsLeft"
+        @focus="($event) => $event.target.select()"
+      />
 
       <button @click="this.startTimer">Start!</button>
     </div>
 
     <div v-else>
       <h2>Time Remaining</h2>
-      <h3>{{ this.notate(this.timeLeft) }} remaining in timer</h3>
+      <h3>{{ notate(timeLeft) }} remaining in timer</h3>
+      <button @click="cancelTimer">Cancel Timer</button>
     </div>
   </div>
 </template>
@@ -27,13 +38,14 @@ import Component from "vue-class-component";
 export default class Timer extends Vue {
   // DATA
   timerId: any = null;
-  secondsLeft = 0;
-  minutesLeft = 0;
+  secondsLeft = "0";
+  minutesLeft = "0";
   timeLeft = 0;
 
   // METHODS
   startTimer() {
-    this.timeLeft = 60 * this.minutesLeft + this.secondsLeft;
+    this.timeLeft =
+      60 * parseInt(this.minutesLeft) + parseInt(this.secondsLeft);
     this.timerId = setInterval(() => {
       if (this.timeLeft === 0) {
         alert("Time's up!");
@@ -60,6 +72,14 @@ export default class Timer extends Vue {
     }
 
     return `${minutes} minutes ${seconds} seconds`;
+  }
+
+  cancelTimer() {
+    clearInterval(this.timerId);
+    this.timerId = null;
+    this.secondsLeft = "";
+    this.minutesLeft = "";
+    this.timeLeft = 0;
   }
 }
 </script>
